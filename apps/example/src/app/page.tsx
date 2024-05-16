f5366ca57ca67e008b40510d1d207a0679541e60
zeevo <8338013+zeevo@users.noreply.github.com>
2024-05-15 20:17:02 -0400

    use force-dynamic in example app

--- apps/example/src/app/page.tsx
+++ apps/example/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { NextPostgrestSwagger } from "next-postgrest";
 import { Swagger } from "./_components/swagger";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const spec = await NextPostgrestSwagger({
     url: "http://localhost:3333",
